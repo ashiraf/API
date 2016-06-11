fd2309a45fe71cbebee32440e07bf75d651c470f
Ashirafali Omary <ashirafali.omary@ega,go,tz>
2016-06-11 13:38:14 -0700

    DB connection

---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php
+$con = mysql_connect('localhost', 'root', '');
+if(!$con){
+die("Database connection failed ".mysql_error());
+}
+
+$db = mysql_select_db('code_test', $con);
+if(!$db){
+die("Database selection failed ".mysql_error());
+}
+
+defined("SITE_ROOT")? null : define("SITE_ROOT","http://127.0.0.1/eagle");
+
+?>
